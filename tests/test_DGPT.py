138541033853bd0f5cddeaeeb55aeafa23efd41e
Mohsen Mostafa <149787410+MohsenMostafa1@users.noreply.github.com>
2025-01-26 12:07:25 +0200

    Add files via upload

--- tests/test_DGPT.py
+++ tests/test_DGPT.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the project root directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from fastapi.testclient import TestClient
 from main import app  # Import your FastAPI app
 
